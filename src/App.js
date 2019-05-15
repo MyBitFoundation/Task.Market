ae58d10790b5baa6e696ef605ce83f32c297924e
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2019-05-16 00:19:50 +0100

    updated the version of the header and changed the required text

--- src/App.js
+++ src/App.js
@@ -80,18 +80,47 @@ const TelegramButton = styled.a`
   width: max-content;
 `
 
+const StyledLink = styled.a`
+  color: #ffffff;
+  text-decoration: none;
+  background-color: transparent;
+  outline: none;
+  cursor: pointer;
+
+  &:hover{
+    text-decoration: none;
+    outline: 0;
+    color: #40a9ff;
+  }
+`
+
 function App() {
   return (
     <Root>
-      <Header/>
+      <Header
+        title="Task.Market is being replaced"
+        content={<span>During our quest to design an application for decentralised workflows 
+          we connected with Autark Labs and The Planning Suite (
+            <StyledLink
+              href="https://github.com/AutarkLabs/planning-suite"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              TPS
+            </StyledLink>
+          ), which offers 
+          the ideal solution for MyBit. Therefore, we decided to work together, rather 
+          than compete with our own application.</span>}
+      />
       <div className="content">
         <LandingWrapper>
           <LandingIcon />
           <LandingTitle>Opportunity to Create</LandingTitle>
           <LandingParagraph>
-            As such, we see this as a giant opportunity to create our own version on 
-            Bounties.Network that maintains the values and principles of the industry as well as 
-            maintains the best interests of the users.
+            TPS is an Aragon application which seamlessly integrates into the MyBit DAO. 
+            They have received nearly $500,000 in funding from the Aragon Foundation to develop their 
+            solution. Currently, it is still on testnet, but we have fully tested it and are ready to 
+            bring it to the MyBit DAO the second it reaches main-net.
           </LandingParagraph>
           <LandingTagline>More to come shortly. Join Telegram for updates.</LandingTagline>
           <TelegramButton
